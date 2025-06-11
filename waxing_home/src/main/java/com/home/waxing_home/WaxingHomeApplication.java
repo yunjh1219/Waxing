d4e68719ac4fc47ec4090b80fde5cd801173a557
yunjh1219 <orenjh1219@gmail.com>
2025-06-11 16:59:38 +0900

    first commit

---
+++ waxing_home/src/main/java/com/home/waxing_home/WaxingHomeApplication.java
@@ -0,0 +1,13 @@
+package com.home.waxing_home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaxingHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaxingHomeApplication.class, args);
+    }
+
+}
---
+++ waxing_home/src/test/java/com/home/waxing_home/WaxingHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.waxing_home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaxingHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
